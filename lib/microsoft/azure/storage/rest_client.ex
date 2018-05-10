47caff7039b5a427852c690b5a31058b2ccf7360
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-05-11 00:39:15 +0200

    http_proxy environment variable leverage

--- lib/microsoft/azure/storage/rest_client.ex
+++ lib/microsoft/azure/storage/rest_client.ex
@@ -3,18 +3,39 @@ defmodule Microsoft.Azure.Storage.RestClient do
 
   adapter(:ibrowse)
 
+  def proxy_middleware() do
+    case System.get_env("http_proxy") do
+      nil ->
+        []
+
+      "" ->
+        []
+
+      proxy_cfg ->
+        proxy_cfg
+        |> String.split(":")
+        |> (fn [host, port] ->
+              [
+                {Tesla.Middleware.Opts,
+                 [
+                   proxy_host: host |> String.to_charlist(),
+                   proxy_port: port |> Integer.parse() |> elem(0)
+                 ]}
+              ]
+            end).()
+    end
+  end
+
   def new(base_url) when is_binary(base_url) do
-    Tesla.build_client([
-      {Tesla.Middleware.BaseUrl, base_url},
-      {Tesla.Middleware.Opts, [proxy_host: '127.0.0.1', proxy_port: 8888]}
-    ])
+    Tesla.build_client([{Tesla.Middleware.BaseUrl, base_url}] ++ proxy_middleware())
   end
 
   def new(base_url, headers) when is_binary(base_url) and is_map(headers) do
-    Tesla.build_client([
-      {Tesla.Middleware.BaseUrl, base_url},
-      {Tesla.Middleware.Headers, headers},
-      {Tesla.Middleware.Opts, [proxy_host: '127.0.0.1', proxy_port: 8888]}
-    ])
+    Tesla.build_client(
+      [
+        {Tesla.Middleware.BaseUrl, base_url},
+        {Tesla.Middleware.Headers, headers}
+      ] ++ proxy_middleware()
+    )
   end
 end
