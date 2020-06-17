9bf5f3036e0659fdba189d4e157cc4f5cbbfc0cd
Peter Arentsen <peter.arentsen@holder.nl>
2020-06-17 12:57:56 +0200

    feat: add ability to set different host for development mode

--- lib/microsoft/azure/storage/storage.ex
+++ lib/microsoft/azure/storage/storage.ex
@@ -1,13 +1,12 @@
 defmodule Microsoft.Azure.Storage do
   @derive {Inspect, except: [:account_key]}
   @enforce_keys []
-  defstruct [
-    :account_name,
-    :account_key,
-    :aad_token_provider,
-    :cloud_environment_suffix,
-    :is_development_factory
-  ]
+  defstruct account_name: nil,
+            account_key: nil,
+            host: nil,
+            aad_token_provider: nil,
+            cloud_environment_suffix: nil,
+            is_development_factory: false
 
   @endpoint_names %{
     blob_service: "blob",
@@ -21,20 +20,22 @@ defmodule Microsoft.Azure.Storage do
   @doc """
   Returns the storage context for the Azure storage emulator.
   """
-  def development_factory(),
-    do: %__MODULE__{
+  def development_factory(host \\ "127.0.0.1") do
+    %__MODULE__{
       # https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator#authenticating-requests-against-the-storage-emulator
       account_name: "devstoreaccount1",
       account_key: @development_fabric_key,
+      host: host,
       is_development_factory: true
     }
+  end
 
   def secondary(context = %__MODULE__{}),
     do:
       context
       |> Map.update!(:account_name, &(&1 <> "-secondary"))
 
-  def endpoint_url(context = %__MODULE__{is_development_factory: true}, service)
+  def endpoint_url(context = %__MODULE__{is_development_factory: true, host: host}, service)
       when is_atom(service) do
     port =
       case service do
@@ -43,7 +44,7 @@ defmodule Microsoft.Azure.Storage do
         :table_service -> 10002
       end
 
-    %URI{scheme: "http", host: "127.0.0.1", port: port, path: "/" <> context.account_name}
+    %URI{scheme: "http", host: host, port: port, path: "/" <> context.account_name}
     |> URI.to_string()
   end
 
