8b83eaa789369b9a7be6089576eec079fd9e01b0
Peter Arentsen <peter.arentsen@holder.nl>
2020-11-30 16:16:09 +0100

    chore: remove some options that aren't valid

--- lib/microsoft/azure/storage/blob.ex
+++ lib/microsoft/azure/storage/blob.ex
@@ -205,7 +205,7 @@ defmodule Microsoft.Azure.Storage.Blob do
         end
 
         filename
-        |> File.stream!([:read_ahead, :binary], block_size)
+        |> File.stream!([], block_size)
         |> Stream.zip(1..50_000)
         |> Task.async_stream(
           fn {content, i} ->
