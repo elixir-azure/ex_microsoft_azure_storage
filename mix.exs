956169a74b568b663ae95ac372c2a6dc46abcefd
nulian <peter.arentsen@quicknet.nl>
2022-11-22 11:24:52 +0100

    chore: bump version number

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExMicrosoftAzureStorage.MixProject do
   use Mix.Project
 
-  @version "1.1.0"
+  @version "1.1.1"
   @repo_url "https://github.com/bettyblocks/ex_microsoft_azure_storage"
 
   def project do
